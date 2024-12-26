19adafaedf860be2c55d1d6e665c7717d6fd4166
pastyk <tsarev.kr@ya.ru>
2024-12-26 09:21:16 +0300

    gen3

---
+++ src/CircleTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+        double radius = 5;
+    }
+}
---
+++ src/org/example/MainS.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MainS {
+    public static void main(String[] args) {
+
+    }
+
+    public static double Sum(double num1, double num2) {
+        return num1+num2;
+    }
+}
